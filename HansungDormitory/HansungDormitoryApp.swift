c6084e2098827575ce1347020d5386325a06cdfa
soohee Jang <shjang0820@hansung.ac.kr>
2024-07-03 15:33:24 +0900

    Initial Commit

---
+++ HansungDormitory/HansungDormitoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HansungDormitoryApp.swift
+//  HansungDormitory
+//
+//  Created by JangSoohee on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct HansungDormitoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
