f5cac1e8c3b2ea11f2f0287debb3a1d531102566
soohee Jang <shjang0820@hansung.ac.kr>
2024-07-07 19:26:14 +0900

    Add BottomTab on ContentView

--- HansungDormitory/ContentView.swift
+++ HansungDormitory/ContentView.swift
@@ -1,24 +1,39 @@
-//
-//  ContentView.swift
-//  HansungDormitory
-//
-//  Created by JangSoohee on 7/3/24.
-//
-
+// ContentView.swift
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+            
+            Text("Note View")
+                .tabItem {
+                    Image(systemName: "pencil.tip.crop.circle")
+                        .font(.system(size: 24, weight: .bold))
+                    Text("Note")
+                }
+            
+            Text("Check View")
+                .tabItem {
+                    Image(systemName: "checkmark.square")
+                    Text("Check")
+                }
+            
+            Text("Profile View")
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Profile")
+                }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
