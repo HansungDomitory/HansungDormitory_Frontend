ef171555284ad4edec195f40bad4b2fcf47821fb
soohee Jang <shjang0820@hansung.ac.kr>
2024-07-08 17:14:24 +0900

    Add NoticeView Design

--- HansungDormitory/NoticeView.swift
+++ HansungDormitory/NoticeView.swift
@@ -6,3 +6,105 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct NoticeView: View {
+    let notices = [
+        Notice(title: "[상상빌리지] 하계 방중 행정실 근무시간...", department: "행정실", date: "2024-06-19"),
+        Notice(title: "[상상빌리지] 동계 방중 행정실 근무시간...", department: "행정실", date: "2024-06-19"),
+        Notice(title: "[상상빌리지] 하계 방중 행정실 근무시간...", department: "행정실", date: "2024-06-19")
+    ]
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            // Header
+            HStack {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 100, height: 30)
+                    .padding(.leading)
+                Spacer()
+            }
+            .padding(.vertical)
+            
+            // Title
+            HStack {
+                Spacer()
+                Text("공지사항")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding(.horizontal)
+            .padding(.bottom, 20)
+            // Notice Count and Search Bar
+            HStack {
+                Text("total: \(notices.count)건")
+                    .padding(.leading)
+                Spacer()
+                TextField("검색", text: .constant(""))
+                    .padding(8)
+                    .background(Color(UIColor.systemGray6))
+                    .cornerRadius(8)
+                    .padding(.trailing, 5)
+                Button(action: {
+                    // 검색 버튼 액션
+                }) {
+                    Text("검색")
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 7)
+                        .padding(.vertical, 6)
+                        .background(Color.gray)
+                        .cornerRadius(3)
+                }
+                .padding(.trailing, 5)
+            }
+            .padding(.vertical, 5)
+            .padding(.bottom, 20)
+            
+            Divider()
+            
+            // Notice List
+            ScrollView {
+                VStack(alignment: .leading, spacing: 0) {
+                    ForEach(notices, id: \.self) { notice in
+                        VStack(alignment: .leading) {
+                            Text(notice.title)
+                                .font(.headline)
+                                .padding(.horizontal)
+                                .padding(.vertical, 5)
+                            HStack {
+                                Text(notice.department)
+                                    .font(.subheadline)
+                                    
+                                Spacer()
+                                Text(notice.date)
+                                    .font(.subheadline)
+
+                            }
+                            .padding(.horizontal)
+                            Divider()
+                        }
+                    }
+                }
+            }
+            
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct Notice: Identifiable, Hashable {
+    let id = UUID()
+    let title: String
+    let department: String
+    let date: String
+}
+
+struct NoticeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoticeView()
+    }
+}
+
