790a05dc3fd6543f9b3a5eec3235cf3c9c59db21
soohee Jang <shjang0820@hansung.ac.kr>
2024-08-06 03:57:56 +0900

    Update HomeView
    홈화면에서 공지사항 관련 기능 추가

--- HansungDormitory/HomeView.swift
+++ HansungDormitory/HomeView.swift
@@ -9,74 +9,86 @@ import Foundation
 import SwiftUI
 
 struct HomeView: View {
+    @StateObject private var viewModel = NoticeViewModel()
+    
     var body: some View {
+
         NavigationView {
-            VStack(spacing: 0) {
-                // Header
-                HStack {
-                    Image("logo")
-                        .resizable()
-                        .frame(width: 100, height: 30)
-                        .padding(.leading)
-                    Spacer()
-                }
-                .padding(.vertical)
-                
-                
-                // Main Image
-                Image("dormitoryImage")
-                    .resizable()
-                    .scaledToFit()
-                
-                // Dormitory Info
-                VStack(alignment: .leading, spacing: 10) {
+            ScrollView(){
+                VStack(spacing: 0) {
+                    // Header
                     HStack {
-                        Text("기숙사 소개")
-                            .font(.headline)
+                        Image("logo")
+                            .resizable()
+                            .frame(width: 100, height: 30)
                             .padding(.leading)
                         Spacer()
-                        NavigationLink(destination: InfoView()) {
-                            Image(systemName: "plus")
-                                .foregroundColor(.black)
-                                .padding(.trailing)
+                    }
+                    .padding(.vertical)
+                    
+                    
+                    // Main Image
+                    Image("dormitoryImage")
+                        .resizable()
+                        .scaledToFit()
+                    
+                    // Dormitory Info
+                    VStack(alignment: .leading, spacing: 10) {
+                        HStack {
+                            Text("기숙사 소개")
+                                .font(.headline)
+                                .padding(.leading)
+                            Spacer()
+                            NavigationLink(destination: InfoView()) {
+                                Image(systemName: "plus")
+                                    .foregroundColor(.black)
+                                    .padding(.trailing)
+                            }
+                        }
+                        .padding(.vertical, 5)
+                        Divider()
+                        Text("한성대학교 상상빌리지는 쾌적하고 안전한 환경을 제공합니다. 최신 시설과 편리한 생활 공간을 갖춘 기숙사는 학생들의 학습과 생활을 지원합니다. 다양한 편의 시설과 함께 활기찬 커뮤니티를 경험해 보세요.")
+                            .font(.system(size: 15))
+                            .padding(.horizontal)
+                            .padding(.bottom, 10)
+                    }
+                    .background(Color(UIColor.systemGray6))
+                    .padding()
+                    
+                    // Notices
+                    VStack(alignment: .leading, spacing: 6) {
+                        HStack {
+                            Text("공지사항")
+                                .font(.headline)
+                                .padding(.leading)
+                            Spacer()
+                            NavigationLink(destination: NoticeView()) {
+                                Image(systemName: "plus")
+                                    .foregroundColor(.black)
+                                    .padding(.trailing)
+                            }
+                        }
+                        .padding(.vertical, 5)
+                        Divider()
+                        ForEach(viewModel.notices.prefix(3), id: \.self) { notice in
+                            Text(notice.title)
+                                .padding(.horizontal)
+                                .padding(.vertical, 3)
+                                .font(.system(size: 15))
+                            Divider()
                         }
                     }
-                    .padding(.vertical, 5)
-                    Divider()
-                    Text("한성대학교 상상빌리지는 쾌적하고 안전한 환경을 제공합니다. 최신 시설과 편리한 생활 공간을 갖춘 기숙사는 학생들의 학습과 생활을 지원합니다. 다양한 편의 시설과 함께 활기찬 커뮤니티를 경험해 보세요.")
-                        .padding(.horizontal)
-                        .padding(.bottom, 10)
+                    .background(Color(UIColor.systemGray6))
+                    .padding(.horizontal)
+                    
+                    Spacer()
                 }
-                .background(Color(UIColor.systemGray6))
                 .padding()
-                
-                // Notices
-                VStack(alignment: .leading, spacing: 10) {
-                    HStack {
-                        Text("공지사항")
-                            .font(.headline)
-                            .padding(.leading)
-                        Spacer()
-                        Image(systemName: "plus")
-                            .padding(.trailing)
-                    }
-                    .padding(.vertical, 5)
-                    Divider()
-                    ForEach(0..<3) { _ in
-                        Text("[상상빌리지] 하계 방중 행정실 근무시간...")
-                            .padding(.horizontal)
-                            .padding(.vertical, 3)
-                        Divider()
-                    }
-                }
-                .background(Color(UIColor.systemGray6))
-                .cornerRadius(10)
-                .padding(.horizontal)
-                
-                Spacer()
+                .navigationBarHidden(true)
             }
-            .padding()
-            .navigationBarHidden(true)
+            .onAppear {
+                viewModel.fetchNotices()
+            }
         }
     }
 }
